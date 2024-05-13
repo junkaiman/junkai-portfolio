cf84a4e87e3ba2fff249f3a7654d4d04a5003a39
Junkai Man <60953254+junkaiman@users.noreply.github.com>
2024-05-13 21:07:40 +0800

    Improve the mdx try catch logic

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -2,17 +2,13 @@ import dynamic from "next/dynamic";
 import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const Content = await getMdxContent(params.slug);
-
-  return <Content />;
-}
-
-async function getMdxContent(slug: string) {
-  return dynamic(async () => {
+  const Content = dynamic(async () => {
     try {
-      return await import(`@/_contents/projects/${slug}.mdx`);
+      return await import(`@/_contents/projects/${params.slug}.mdx`);
     } catch (error) {
       notFound();
     }
   });
+
+  return <Content />;
 }
