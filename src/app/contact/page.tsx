ae81dc4ba2feae34483cd72048093b4a0248386b
Junkai Man <v-junkaiman@microsoft.com>
2024-05-14 16:03:25 +0800

    Add home, contact, projects pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,13 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-export default function ProjectPage() {
+export default function ContactPage() {
   return (
     <>
       <Header />
-      <h1>Project page</h1>
+      <h1>Contact Page</h1>
+      <p>This is the contact page.</p>
       <Footer />
     </>
-  )
+  );
 }
