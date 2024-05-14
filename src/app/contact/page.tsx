1f598a7ca64f0200d0181fa7fa65ab1f5c3d4be0
Junkai Man <60953254+junkaiman@users.noreply.github.com>
2024-05-14 23:15:42 +0800

    Update about page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,21 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Link from "next/link";
+import basic from "@/_contents/basic.json";
 
 export default function ContactPage() {
   return (
     <>
       <Header />
-      <h1>Contact Page</h1>
-      <p>This is the contact page.</p>
+      <div>
+        {"Hi there! 👋 You can see what I'm up to on "}
+        <Link href={basic.socials.instagram}>Instagram</Link>
+        {", connect with me on "}
+        <Link href={basic.socials.linkedin}>Linkedin</Link>
+        {", or directly send me "}
+        <Link href={`mailto:${basic.socials.email}`}>Email</Link>
+        {"."}
+      </div>
       <Footer />
     </>
   );
