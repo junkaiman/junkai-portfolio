8c9634c0ec93f556322166cc4c457760a3ff64aa
Junkai Man <v-junkaiman@microsoft.com>
2024-05-17 17:51:50 +0800

    Update

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,7 +3,7 @@ import SelfIntro from "@/_contents/about/self-intro.mdx";
 export default function AboutMe() {
   return (
     <div className="p-5 flex flex-row">
-      <div className="px-4">
+      <div className="px-4 text-xl space-y-4">
         <SelfIntro />
       </div>
       <div>personal pic on the right</div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
 import basic from "@/_contents/basic.json";
+import style from "./Header.module.css";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between p-4">
       <Link href="/" className="items-center">
         <Image src={basic.siteIcon} alt="logo" width={45} height={45} />
-        {/* <h1 className="text-2xl font-bold">{basic.name}</h1> */}
       </Link>
       <nav>
-        <ul className="flex space-x-4">
+        {/* TODO: Highlight current page */}
+        <ul className={style.navContainer + " flex space-x-4 text-lg"}>
           <li>
             <Link href="/projects">Projects</Link>
           </li>
